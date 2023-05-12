5f53d16c0b7a1b5476cd21c2791ce2b00b40c38f
User <godzorox@gmail.com>
2023-05-12 10:34:35 -0300

    Implementada direção

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -8,6 +8,14 @@ class Pessoa:
     def cumprimentar(self):
         return f'Olá {id (self)}'
 
+    @staticmethod
+    def metodo_estatico():
+        return 42
+
+    @classmethod
+    def nome_e_atributos_de_classe(cls):
+        return f'{cls} -olhos {cls.olhos}'
+
 if __name__=='__main__':
     mateus = Pessoa(nome='Mateus')
     luciano = Pessoa(mateus, nome='Luciano')
@@ -29,3 +37,5 @@ if __name__=='__main__':
     print(luciano.olhos)
     print(mateus.olhos)
     print(id(Pessoa.olhos), id(luciano.olhos), id(mateus.olhos))
+    print(Pessoa.metodo_estatico(), luciano.metodo_estatico())
+    print(Pessoa.nome_e_atributos_de_classe(), luciano.nome_e_atributos_de_classe())
