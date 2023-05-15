596dd1a15871252df9083b3eaccca1b062c6c713
User <godzorox@gmail.com>
2023-05-15 10:31:36 -0300

    Implementada direção

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,12 +1,12 @@
 class Pessoa:
     olhos = 2
 
-    def __init__(self, *filhos, nome=None, idade=35):
+    def __init__(self, *filhos, nome=None, idade=27):
         self.idade = idade
         self.nome = nome
         self.filhos = list(filhos)
     def cumprimentar(self):
-        return f'Olá {id (self)}'
+        return f'Olá meu nome é {self.nome}'
 
     @staticmethod
     def metodo_estatico():
@@ -16,9 +16,16 @@ class Pessoa:
     def nome_e_atributos_de_classe(cls):
         return f'{cls} -olhos {cls.olhos}'
 
+class Homem(Pessoa):
+    def cumprimentar(self):
+        cumprimentar_da_classe=super().cumprimentar()
+        return f'{cumprimentar_da_classe}. Aperto de mão'
+class Mutante(Pessoa):
+    olhos = 3
+
 if __name__=='__main__':
-    mateus = Pessoa(nome='Mateus')
-    luciano = Pessoa(mateus, nome='Luciano')
+    mateus = Mutante(nome='Mateus')
+    luciano = Homem(mateus, nome='Luciano')
     print(Pessoa.cumprimentar(luciano))
     print(id(luciano))
     print(luciano.cumprimentar())
@@ -32,10 +39,17 @@ if __name__=='__main__':
     del luciano.olhos
     print(luciano.__dict__)
     print(mateus.__dict__)
-    Pessoa.olhos = 3
     print(Pessoa.olhos)
     print(luciano.olhos)
     print(mateus.olhos)
     print(id(Pessoa.olhos), id(luciano.olhos), id(mateus.olhos))
     print(Pessoa.metodo_estatico(), luciano.metodo_estatico())
     print(Pessoa.nome_e_atributos_de_classe(), luciano.nome_e_atributos_de_classe())
+    pessoa = Pessoa('Anonimo')
+    print(isinstance(pessoa, Pessoa))
+    print(isinstance(pessoa, Homem))
+    print(isinstance(mateus, Pessoa))
+    print(isinstance(mateus, Pessoa))
+    print(mateus.olhos)
+    print(luciano.cumprimentar())
+    print(mateus.cumprimentar())
